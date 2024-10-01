66339631173619a69b2c79b89481bdce07d5b56c
user <17032006daniil@gmail.com>
2024-10-01 08:58:49 +0300

    first commit

--- week1/task1/src/task1.cpp
+++ week1/task1/src/task1.cpp
@@ -1,21 +1,17 @@
 #include <iostream>
-#include <cmath>
+#include <string>
 
 int main() {
-    double a, volume;
+    std::string userName;
+    
+    std::cout << "Введите ваше имя: ";
+    std::cin >> userName;
 
-    // Запрашиваем у пользователя длину ребра икосаэдра
-    std::cout << "Введите длину ребра икосаэдра: ";
-    std::cin >> a;
-
-    // Вычисляем объем икосаэдра
-    volume = (5.0 / 12.0) * (3 + sqrt(5)) * pow(a, 3);
-
-    // Выводим промежуточные расчеты и результат
-    std::cout << "Промежуточные расчеты:\n";
-    std::cout << "Сторона в кубе: " << pow(a, 3) << std::endl;
-    std::cout << "Коэффициент икосаэдра: " << (5.0 / 12.0) * (3 + sqrt(5)) << std::endl;
-    std::cout << "Объем икосаэдра: " << volume << std::endl;
+    // Формирование приветственной строки
+    std::string greeting = "Привет, " + userName + "!";
+    
+    // Вывод приветственной строки
+    std::cout << greeting << std::endl;
 
     return 0;
 }
--- week1/task3/src/task3.cpp
+++ week1/task3/src/task3.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
-#include <string>
+#include <cmath>
 
 int main() {
-    std::string userName;
-    
-    std::cout << "Введите ваше имя: ";
-    std::cin >> userName;
+    double a, volume;
 
-    // Формирование приветственной строки
-    std::string greeting = "Привет, " + userName + "!";
-    
-    // Вывод приветственной строки
-    std::cout << greeting << std::endl;
+    // Запрашиваем у пользователя длину ребра икосаэдра
+    std::cout << "Введите длину ребра икосаэдра: ";
+    std::cin >> a;
+
+    // Вычисляем объем икосаэдра
+    volume = (5.0 / 12.0) * (3 + sqrt(5)) * pow(a, 3);
+
+    // Выводим промежуточные расчеты и результат
+    std::cout << "Промежуточные расчеты:\n";
+    std::cout << "Сторона в кубе: " << pow(a, 3) << std::endl;
+    std::cout << "Коэффициент икосаэдра: " << (5.0 / 12.0) * (3 + sqrt(5)) << std::endl;
+    std::cout << "Объем икосаэдра: " << volume << std::endl;
 
     return 0;
 }
